dbb4ed6b4ad65ff15fa4567a66457c2f385e0075
Ekram Ehsas <ekramehsas@MacBookPro.communityfibre.co.uk>
2026-03-23 22:59:37 +0000

    New Opening Date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@
       style="background-color: {NAVY_DARK}; color: {CREAM} border-bottom: 1px solid rgba(255,249,229,0.18);"
       
     >
-      WE OPEN 28 MARCH • 10AM
+      OPENING 4 APRIL • 10AM
     </div>
         
     <div class="relative mx-auto flex max-w-6xl items-center px-5 py-6">
